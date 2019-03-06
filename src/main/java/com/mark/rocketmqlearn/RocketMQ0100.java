2338a81b6d1ec84d14da77de908c1f40413943fa
Mark <18009838166@163.com>
2019-03-06 20:24:37 +0800

    format code style and add code formatter files.

--- src/main/java/com/mark/rocketmqlearn/RocketMQ0100.java
+++ src/main/java/com/mark/rocketmqlearn/RocketMQ0100.java
@@ -4,10 +4,8 @@ package com.mark.rocketmqlearn;
  * Hello world!
  *
  */
-public class RocketMQ0100 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class RocketMQ0100 {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
 }
